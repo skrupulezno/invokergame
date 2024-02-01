4868f456e34cdce208c33517e304de3f968df3d5
geraldop <geraldop@student.21-school.ru>
2024-02-02 02:14:21 +0900

    added task n timer

--- src/script.js
+++ src/script.js
@@ -1,14 +1,22 @@
-sphere_1 = document.getElementById('sphere_1');
-sphere_2 = document.getElementById('sphere_2');
-sphere_3 = document.getElementById('sphere_3');
+const sphere_1 = document.getElementById('sphere_1');
+const sphere_2 = document.getElementById('sphere_2');
+const sphere_3 = document.getElementById('sphere_3');
 
-d = document.getElementById('d');
-f = document.getElementById('f');
+const btnStart = document.getElementById("btnStart");
 
-s1 = document.getElementById('skill1');
-s2 = document.getElementById('skill2');
+const taskElement = document.getElementById("task");
+const taskImage = document.getElementById("task__image");
+
+const d = document.getElementById('d');
+const f = document.getElementById('f');
+
+const s1 = document.getElementById('skill1');
+const s2 = document.getElementById('skill2');
 
 let lastSpell = 0;
+let countCards = 0;
+let canPlay = false;
+let cardSpellId = -1;
 
 const skillsMas = [
   {id: 1, name: "Cold Snap", src: "cold-snap.jpg", pool: [3,0,0]},
@@ -38,11 +46,14 @@ document.addEventListener('keydown', function(e){
 		invoke();
 	}
 	else if (e.code === 'Space') {
-		invoke();
+		startGame();
 	}
 });
 
 function rollBack(btn, imageSrc) {
+	if(!canPlay) {
+		return;
+	}
 	skillPool = [0,0,0];
 	sphere_3.textContent = sphere_2.textContent;
   sphere_2.textContent = sphere_1.textContent;
@@ -99,47 +110,75 @@ function invoke() {
 	if (item == -1) {
 		return;
 	}
-	console.log(lastSpell)
-	if (item != lastSpell) {
+	if (item != lastSpell && countCards < 10) {
 		rollBackSkill(item);
+		if (item + 1 == cardSpellId) {
+			nextCard();
+		}
 	}
-	
+}
+
+function nextCard() {
+	countCards++;
+	previousId = setRandomValues(previousId);
 }
 
 function startGame() {
-	
+	startTimer();
+	previousId = setRandomValues(-1);
+	countCards = 0;
+	btnStart.disabled = true; 
+	canPlay = true;
 }
 
-let seconds = 0;
-let milliseconds = 0;
-let intervalId;
-let stopTimer = true;
+//*************
+let intervalVariable = undefined;  
+let timeleft = 0;  
+let totaltime = 0;  
 
-function start() {
-	if (!stopTimer) {
-    stopTimer = true;
-  }
-	stopTimer = false;
-  intervalId = setInterval(updateTimer, 10); // Обновление таймера каждые 10 миллисекунд
-}
+const startTimer = () => {  
+   // Создаем интервал, который будет считать нам время
+   intervalVariable = setInterval(updateTime, 10);  
+}  
 
-function updateTimer() {
-	if (stopTimer) {
-		clearInterval(intervalId);
+ const resetTimer = () => {  
+   // Останавливаем таймер
+   stopTimer();
+   timeleft = -10;
+   
+   // Обновляем значение в ui
+   updateTime();  
+}  
+
+const stopTimer = () => {
+   // Производим очистку интервала
+   clearInterval(intervalVariable);  
+} 
+
+async function updateTime() {
+  // Шаг 10 миллисекунд
+  timeleft = timeleft + 10;  
+  let milli = timeleft % 1000;  
+
+	if (Math.floor(timeleft / 1000) > 100 || countCards >= 10) {
+		stopTimer();
+		taskImage.style.display = "none";
+		console.log("end "+ countCards);
+		document.getElementById("timer").innerText = `${Math.floor(timeleft / 1000)}.${Math.floor(milli / 10)} sec`;
 	}
-  milliseconds += 10; 
+}  
 
-  if (milliseconds === 1000) {
-    // Если достигли 1000 миллисекунд (1 секунда), сбрасываем миллисекунды и увеличиваем секунды
-    milliseconds = 0;
-    seconds++;
+
+function setRandomValues(previousId) {
+  let randomIndex = Math.floor(Math.random() * skillsMas.length);
+  if (previousId !== -1) {
+    while (skillsMas[randomIndex].id === previousId) {
+      randomIndex = Math.floor(Math.random() * skillsMas.length);
+    }
   }
-
-  if (seconds === 100) {
-    // Если достигли 2 секунд, останавливаем таймер
-    clearInterval(intervalId);
-  }
-
-  // Обновляем текст элемента с id "timer" на текущее значение секунд и миллисекунд
-  //document.getElementById("timer").innerText = `${seconds}:${Math.floor(milliseconds / 100)}`;
+  const randomSkill = skillsMas[randomIndex];
+  taskImage.src = 'images/' + randomSkill.src;
+  taskElement.textContent = randomSkill.name;
+	cardSpellId = randomSkill.id;
+  return randomSkill.id;
 }
